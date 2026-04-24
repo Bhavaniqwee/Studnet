5f8cfa3509abccf05f8e606da72842573168968a
bharadwajcharya <ananthojubharadwaj@gmail.com>
2026-04-24 16:06:14 +0530

    subapp created

---
+++ backend/Student/Bharadwaj/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def bharadwaj(request):
+  obj=HttpResponse("login_chary ..........")
+  return obj
+
--- backend/Student/Student/urls.py
+++ backend/Student/Student/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('rasool.urls'))
+    path('', include('rasool.urls')),
+    path('hi/', include('Bharadwaj.urls')),
 ]
